cb4b2451640d864e882a7e2552f72e2781039772
kqcoxn <mjy03730@163.com>
2025-04-23 00:37:46 +0800

    fix: 依赖包加载顺序

--- agent/main.py
+++ agent/main.py
@@ -1,18 +1,16 @@
 import sys
 
-from maa.agent.agent_server import AgentServer
-from maa.toolkit import Toolkit
-
 from setup import check_and_install_dependencies
-
-import actions.ConsumptiveRealism
 
 
 def main():
+    from maa.agent.agent_server import AgentServer
+    from maa.toolkit import Toolkit
+
+    import actions.ConsumptiveRealism
+
     Toolkit.init_option("./")
-
     socket_id = sys.argv[-1]
-
     AgentServer.start_up(socket_id)
     AgentServer.join()
     AgentServer.shut_down()
