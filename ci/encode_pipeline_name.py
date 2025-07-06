268846f408dc3af01d15f5b28354076643f3d642
kqcoxn <mjy03730@163.com>
2025-07-06 15:49:18 +0800

    ci: 编码pipeline文件名为非中文

---
+++ ci/encode_pipeline_name.py
@@ -0,0 +1,24 @@
+from pathlib import Path
+import os
+
+working_dir = Path(__file__).parent.parent
+resource_path = working_dir / "assets" / "resource"
+pipeline_paths = [
+    (file / "pipeline") for file in resource_path.iterdir() if file.is_dir()
+]
+
+
+def encode_dir(dir):
+    count = 1
+    for file in dir.iterdir():
+        if file.is_dir():
+            encode_dir(file)
+        new_name = f"{count:02d}{file.suffix}"
+        file.rename(file.with_name(new_name))
+        count += 1
+        print(f"{file.name} -> {new_name}")
+
+
+if __name__ == "__main__":
+    for pipeline_path in pipeline_paths:
+        encode_dir(pipeline_path)
