c585bff6307c59d912398533fa3d08d19e31b2c2
kqcoxn <mjy03730@163.com>
2025-07-10 02:30:48 +0800

    feat: 结束界面为主页的任务结束后执行“回到主页”任务

--- agent/customs/Pipeliner.py
+++ agent/customs/Pipeliner.py
@@ -5,6 +5,7 @@ from maa.context import Context
 from .utils import parse_query_args, parse_list_input, Prompt
 
 
+# 解析参数
 def parse_pipeline_args(argv: CustomAction.RunArg):
     args = parse_query_args(argv)
     node = args.get("node")
@@ -13,6 +14,7 @@ def parse_pipeline_args(argv: CustomAction.RunArg):
     return node, keys, values
 
 
+# 设置字符串节点
 @AgentServer.custom_action("set_str_node_attrs")
 class SetStrNodeAttrs(CustomAction):
     def run(
@@ -38,6 +40,31 @@ class SetStrNodeAttrs(CustomAction):
             return Prompt.error("设置节点字符串类型属性", e)
 
 
+# 运行节点
+@AgentServer.custom_action("run")
+class Run(CustomAction):
+    def run(
+        self, context: Context, argv: CustomAction.RunArg
+    ) -> CustomAction.RunResult | bool:
+        try:
+            args = parse_query_args(argv)
+            type = args.get("type", "node")
+            key = args.get("key", "")
+
+            if type == "" or key == "":
+                return False
+
+            if type == "task":
+                context.run_task(key)
+            if type == "node":
+                context.run_action(key)
+
+            return True
+        except Exception as e:
+            return Prompt.error("设置节点字符串类型属性", e)
+
+
+# 阻断
 @AgentServer.custom_action("break")
 class Break(CustomAction):
     def run(
